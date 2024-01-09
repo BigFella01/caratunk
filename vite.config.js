df031d629e0edb418dbe5560b278c4da7c85c9fe
Hayden Jenkins <Haydenj637@gmail.com>
2024-01-09 07:59:48 -0600

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/caratunk/",
-  plugins: [react(), eslint()],
+  base: '/caratunk',
+  plugins: [react(), eslint()]
 })
